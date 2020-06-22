3ed69f215b939121af93ab9f302d4ad566ba9295
morganBarnes <noki9551@gmail.com>
2020-06-22 12:02:01 -0500

    Final File

---
+++ OperatorsExerciseTwo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperatorsExerciseTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+
+            var add = a + b;
+            var sub = a - b;
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"The sum of {a} and {b} is {add}");
+            Console.WriteLine($"The result of {a} minus {b} is {sub}");
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            Console.WriteLine("What is the radius of your circle ?");
+            double realRadius;
+            var radius = double.TryParse(Console.ReadLine(), out realRadius);
+            var circleArea = CalculateArea(realRadius);
+
+            Console.WriteLine($"The area of a circle with a radius of {realRadius} is {circleArea}");
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+}
